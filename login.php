339632665aa1a489cc20ca7edcbc649ec4282e18
FelipeRodriguesPorto <49006580+FelipeRodriguesPorto@users.noreply.github.com>
2021-12-04 08:32:56 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php 
+
+include 'db.php';
+
+$usuario = addslashes($_POST['usuario']);
+
+# antes
+$senha = md5($_POST['senha']);
+
+
+$query = "SELECT * FROM usuarios WHERE usuario = '$usuario' and senha = '$senha'";
+
+$consulta = mysqli_query($conexao, $query);
+
+if(mysqli_num_rows($consulta) == 1){
+	//echo 'Login feito com sucesso';
+	session_start();
+	$_SESSION['login'] = true;
+	$_SESSION['usuario'] = $usuario;
+
+	header('location:index.php');
+}
+else{
+	//echo 'ERRADO';
+	header('location:index.php?erro');
+}
